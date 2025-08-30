2a66af913141738b4a8745926a98f6e7b96d60d6
shenyizhou <shenyihzou@126.com>
2025-08-31 00:32:24 +0800

    feat: 添加酒馆斗殴者专长并更新巨人专长效果
    refactor: 删除废弃的武僧子类相关文件
    
    fix: 修复穿刺打击Lua脚本中的伤害类型逻辑
    
    docs: 更新巨人力量与体魄专长的描述文本
    
    style: 清理代码格式并移除多余注释

--- Backup/MonkSubclasses5eCombined/Mods/MonkSubclasses5eCombined/ScriptExtender/Lua/BootstrapServer.lua
+++
@@ -1,4 +0,0 @@
-Debug = false
-
-Ext.Require("SRD_Monk_DamageConvert.lua")
-Ext.Require("SRD_Util_Lib.lua")
--- DownloadMods/13.Outfit/LynNeahMonkOutfit/Mods/LynNeahMonkOutfit/ScriptExtender/Lua/PiercingStrike.lua
+++ DownloadMods/13.Outfit/LynNeahMonkOutfit/Mods/LynNeahMonkOutfit/ScriptExtender/Lua/PiercingStrike.lua
@@ -4,7 +4,7 @@ Debug = false
 -- 徒手攻击穿刺伤害转换
 local function SetPiercingDamageType(entity, event)
     event.Functor.DamageType = "Piercing"
-    
+
     if Debug then
         local entityUuid = entity.Uuid.EntityUuid
         Ext.Utils.PrintWarning("Converting unarmed damage to Piercing for entity: ", entityUuid)
@@ -16,11 +16,15 @@ Ext.Events.DealDamage:Subscribe(function(e)
     local caster = e.Caster
     local weaponType = e.Functor.WeaponType
     local hasPiercingStrike = false
+    local hasDraconicDisciple = false
+    local hasAstralArms = false
 
     if caster ~= nil and caster.Stats ~= nil then
         local casterUuid = caster.Uuid.EntityUuid
-        hasPiercingStrike = caster.ServerCharacter ~= nil and
-            Osi.HasActiveStatus(casterUuid, "PIERCING_STRIKE_ACTIVE") == 1
+		hasPiercingStrike = caster.ServerCharacter ~= nil and Osi.HasActiveStatus(casterUuid, "PIERCING_STRIKE_ACTIVE") == 1
+        hasDraconicDisciple = caster.ServerCharacter ~= nil and Osi.HasPassive(casterUuid, "DraconicStrike_AD") == 1 and (Osi.HasActiveStatus(casterUuid, "DRACONIC_STRIKE_ACID") == 1 or Osi.HasActiveStatus(casterUuid, "DRACONIC_STRIKE_COLD") == 1
+			or Osi.HasActiveStatus(casterUuid, "DRACONIC_STRIKE_FIRE") == 1 or Osi.HasActiveStatus(casterUuid, "DRACONIC_STRIKE_LIGHTNING") == 1 or Osi.HasActiveStatus(casterUuid, "DRACONIC_STRIKE_POISON") == 1)
+		hasAstralArms = caster.ServerCharacter ~= nil and Osi.HasPassive(casterUuid, "AstralSelfArmsUnlock") == 1 and Osi.HasActiveStatus(casterUuid, "USING_ASTRAL_ARMS") == 1
 
         if Debug then
             print('OriginalWeaponType: ', e.Functor.WeaponType)
@@ -30,6 +34,9 @@ Ext.Events.DealDamage:Subscribe(function(e)
     end
 
     if e.Hit.ConditionRolls[1] ~= nil then
+        if not hasDraconicDisciple and not hasAstralArms and not hasPiercingStrike and weaponType == "UnarmedDamage" and e.Hit.ConditionRolls[1].Roll.Roll.RollType == "MeleeUnarmedAttack" then
+			e.Functor.DamageType = e.SpellId.SpellProto.DamageType or "Bludgeoning"
+		end
         -- 处理穿刺打击的情况
         if hasPiercingStrike and weaponType == "UnarmedDamage" and
            e.Hit.ConditionRolls[1].Roll.Roll.RollType == "MeleeUnarmedAttack" then
