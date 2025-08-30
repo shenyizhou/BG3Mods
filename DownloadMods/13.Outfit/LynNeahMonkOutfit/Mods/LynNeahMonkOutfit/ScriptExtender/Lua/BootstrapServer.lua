af97f9e564910c88262b974f7361a785c5da0b52
shenyizhou <shenyihzou@126.com>
2025-08-30 19:04:45 +0800

    feat(武僧装备): 重构穿刺打击系统并更新装备属性
    修复被动技能文件中的语法错误
    重构Lua脚本以改进伤害类型转换逻辑
    更新装备被动技能配置和本地化文本
    移除不再需要的README和安装指南文件

---
+++ DownloadMods/13.Outfit/LynNeahMonkOutfit/Mods/LynNeahMonkOutfit/ScriptExtender/Lua/BootstrapServer.lua
@@ -0,0 +1,3 @@
+Debug = false
+
+Ext.Require("PiercingStrike.lua")
--- DownloadMods/13.Outfit/LynNeahMonkOutfit/Mods/LynNeahMonkOutfit/ScriptExtender/Lua/PiercingStrike.lua
+++ DownloadMods/13.Outfit/LynNeahMonkOutfit/Mods/LynNeahMonkOutfit/ScriptExtender/Lua/PiercingStrike.lua
@@ -1,8 +1,15 @@
+-- 调试开关
+Debug = false
+
 -- 徒手攻击穿刺伤害转换
 local function SetPiercingDamageType(entity, event)
     local entityUuid = entity.Uuid.EntityUuid
     if Osi.HasActiveStatus(entityUuid, "PIERCING_STRIKE_ACTIVE") == 1 then
         event.Functor.DamageType = "Piercing"
+
+        if (Debug) then
+            Ext.Utils.PrintWarning("Converting unarmed damage to Piercing for entity: ", entityUuid)
+        end
     end
 end
 
@@ -11,17 +18,34 @@ Ext.Events.DealDamage:Subscribe(function(e)
     local caster = e.Caster
     local weaponType = e.Functor.WeaponType
     local hasPiercingStrike = false
-    
-    if caster ~= nil and caster.Stats ~= nil then 
+
+    if caster ~= nil and caster.Stats ~= nil then
         local casterUuid = caster.Uuid.EntityUuid
-        hasPiercingStrike = caster.ServerCharacter ~= nil and 
-            Osi.HasPassive(casterUuid, "PiercingStrike_Gloves") == 1 and 
+        hasPiercingStrike = caster.ServerCharacter ~= nil and
             Osi.HasActiveStatus(casterUuid, "PIERCING_STRIKE_ACTIVE") == 1
+
+        if (Debug) then
+            print('OriginalWeaponType: ', e.Functor.WeaponType)
+            print('OriginalDamageType: ', e.Functor.DamageType)
+            print('HasPiercingStrike: ', hasPiercingStrike)
+        end
     end
 
-    if e.Hit.ConditionRolls[1] ~= nil then 
-        if hasPiercingStrike and weaponType == "UnarmedDamage" and 
-           e.Hit.ConditionRolls[1].Roll.Roll.RollType == "MeleeUnarmedAttack" then 
+    if e.Hit.ConditionRolls[1] ~= nil then
+        -- 重要：先处理非穿刺打击的情况，重置为默认伤害类型
+        if not hasPiercingStrike and weaponType == "UnarmedDamage" and
+           e.Hit.ConditionRolls[1].Roll.Roll.RollType == "MeleeUnarmedAttack" then
+            -- 重置为法术的默认伤害类型或钝击伤害
+            e.Functor.DamageType = e.SpellId.SpellProto.DamageType or "Bludgeoning"
+
+            if (Debug) then
+                print('Reset to default damage type: ', e.Functor.DamageType)
+            end
+        end
+
+        -- 然后处理穿刺打击的情况
+        if hasPiercingStrike and weaponType == "UnarmedDamage" and
+           e.Hit.ConditionRolls[1].Roll.Roll.RollType == "MeleeUnarmedAttack" then
             SetPiercingDamageType(caster, e)
         end
     end
